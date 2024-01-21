2e1515e02d4aaf8a5a3c02b1e4be154d23598e69
Deku1712 <tamtvnde160577@fpt.edu.vn>
2024-01-21 15:30:34 +0700

    dark_light p1

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,30 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-
+import { useColorScheme } from '@mui/material/styles'
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
 function App() {
   return (
     <>
+      <ModeToggle />
       <div>ngoc tam</div>
       <Button variant="text">Hello world</Button>
       <Button variant="contained">Hello world</Button>
       <Button variant="outlined">Hello world</Button>
       <br />
-      <AccessAlarmIcon/>
-      <ThreeDRotation/>
+      <AccessAlarmIcon />
+      <ThreeDRotation />
 
     </>
   )
